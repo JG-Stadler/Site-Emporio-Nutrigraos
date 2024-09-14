02c6544fa525f53dddcf07c140286455dc07d506
João Stadler <joaogbstadler@gmail.com>
2024-09-14 11:31:28 -0300

    Menu principal criado

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/estilos-globais.css">
+    <link rel="stylesheet" href="./styles/index.css">
     <title>Nutrigrãos</title>
 </head>
 <body>
-    
+    <header class="menu-principal d-flex align-items-center justify-content-around">
+        <figure class="logo">
+            <img src="images/logo-nutrigraos-branca.png" id="logo-nutrigraos" alt="logo-nutrigrãos">
+        </figure>
+        <nav class="navbar">
+            <ul class="nav-list d-flex align-items-center">
+                <li class="nav-item"><a href="#" class="nav-link">Instagran</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
