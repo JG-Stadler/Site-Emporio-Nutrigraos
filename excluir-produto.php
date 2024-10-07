4888b99a2a070a0c0b95165bb116a59ae7e6f6f8
João Stadler <joaogbstadler@gmail.com>
2024-10-07 11:59:35 -0300

    pagina de exclusão estilizada

--- excluir-produto.php
+++ excluir-produto.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/cadastro-edicao-exclusao.css">
+    <link rel="stylesheet" href="styles/estilos-globais.css">
     <title>Excluir Produto</title>
 </head>
 <body>
