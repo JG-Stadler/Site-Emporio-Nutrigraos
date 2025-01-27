7427ff3c364087884318d1cacd5ed7005d3299b4
jg_stadler <joaogbstadler@gmail.com>
2025-01-27 10:58:43 -0300

    Conexão com o banco de dados corrigida

--- conection.php
+++ conection.php
@@ -1,23 +1,7 @@
-<!-- 
+<?php
     $hostName = "localhost";
     $banco = "nutrigraos";
     $usuario = "root";
     $senha = "";
     $mysqli = new mysqli($hostName,$usuario,$senha,$banco)
- -->
- <?php
-$hostname = "autorack.proxy.rlwy.net";
-$username = "root";
-$password = "GooSUHLjbQRgZMmNLpCfpwicXagpNcKL";
-$port = "29676";
-$databaseName = "railway";
-
-// Criando a conexão
-$mysqli = new mysqli($hostname, $username, $password, $databaseName, $port);
-
-// Verificando a conexão
-if ($mysqli->connect_error) {
-    die("Conexão falhou: " . $mysqli->connect_error);
-}
 ?>
-
